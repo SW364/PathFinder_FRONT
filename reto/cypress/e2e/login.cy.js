17cf73f220f5858c555741af0ec13f201ef3abec
Gianni <117100331+SW364@users.noreply.github.com>
2025-06-04 17:04:48 -0600

    Update login.cy.js

--- reto/cypress/e2e/login.cy.js
+++ reto/cypress/e2e/login.cy.js
@@ -1,27 +1,7 @@
-// cypress/e2e/login.cy.js
-describe('Pruebas de Login en producción', () => {
-  const BASE_URL = 'https://localhost:3000';
-  const USER_EMAIL = 'camagc6@gmail.com';
-  const USER_PASSWORD = '12345678';
-
-  it('Login exitoso con contraseña existente ("Yes")', () => {
-    cy.visit(`${BASE_URL}`);
-    cy.get('#email').type(USER_EMAIL);
-    cy.contains('button', 'Yes').click();
-    cy.get('#password').type(USER_PASSWORD);
-    cy.contains('button', 'Log in').click();
-    cy.url().should('include', '/home');
-    cy.window().its('localStorage.authToken').should('exist');
-  });
-
-  it('Muestra error con credenciales inválidas', () => {
-    cy.visit(`${BASE_URL}`);
-    cy.get('#email').type('email_invalido@test.com');
-    cy.contains('button', 'Yes').click();
-    cy.get('#password').type('contraseña_incorrecta{enter}');
-    cy.contains('button', 'Log in').click();
-    cy.get('.alert.alert-danger.purple-alert', { timeout: 10000 })
-      .should('be.visible')
-      .and('contain.text', 'Incorrect email or password');
+// reto/cypress/e2e/home.cy.js
+describe('Carga inicial de la app', () => {
+  it('Muestra contenido visible en la página principal', () => {
+    cy.visit('/');
+    cy.contains('PathFinder'); // Cambia esto por texto que siempre aparece al inicio
   });
 });
